031d1e3472e4c69c7c7bde9e412520733cbb7fc1
yael96334 <96549663+yael96334@users.noreply.github.com>
2023-05-28 14:30:53 +0300

    server first commit

---
+++ paginationSeverSide/controller/sort.controller.js
@@ -0,0 +1,17 @@
+//Sorting function on the images ket.
+module.exports.sortBy = async (req, res) => {
+    const { value } = req.params
+    const { arr } = req.query
+    try {
+        const arrSort = arr.sort(function (a, b) {
+            let x = a[value]; var y = b[value];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+        return res.status(200).send(arrSort)
+    }
+    catch (e) {
+        let x="kkk";
+        console.log(e);
+        return res.status(400).send(e);
+    }
+}
